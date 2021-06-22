d8c2455d6a6d3d259b7699d78b95311e3aa7beb4
申一波 <j04052689@qq.com>
2021-06-22 09:45:31 +0800

    #init project

---
+++ ShaderKeywordTest/Assets/Scripts/AssetbundleLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetbundleLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/shader");
+        bundle.LoadAsset<Shader>("TestShader");
+
+        bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/blue");
+        var prefab = bundle.LoadAsset<GameObject>("Blue");
+        var blue = GameObject.Instantiate(prefab);
+        blue.transform.position = Vector3.zero;
+
+        bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/red");
+        prefab = bundle.LoadAsset<GameObject>("Red");
+        var red = GameObject.Instantiate(prefab);
+        red.transform.position = new Vector3(1,0,0);
+
+        bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/green");
+        prefab = bundle.LoadAsset<GameObject>("Green");
+        var green = GameObject.Instantiate(prefab);
+        green.transform.position = new Vector3(-1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ShaderKeywordTest/Assets/Scripts/Editor/BuildAssetbundleTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetbundleTools
+{
+    [MenuItem("Tools/Build Assetbundle")]
+    public static void BuildAssetbundle()
+    {
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath,BuildAssetBundleOptions.None,BuildTarget.Android);
+    }
+}
